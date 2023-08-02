60b8d4619ca6fcbf5688c896af7d40a87bde9d04
ClaraR93 <>
2023-08-02 11:28:52 +0000

    Add changes to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-function initMap() {
+/* function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
         center: {
@@ -8,3 +8,34 @@ function initMap() {
         });
     return map;
 };
+*/
+
+let map;
+
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: 51.86374135992957, lng: - 3.356298099532363 },
+        zoom: 10,
+    });
+
+    let markerOptions = {
+        position: new google.maps.LatLng(51.887444971775096, -3.437991156270883),
+
+    }
+
+    let marker = new google.maps.Marker(markerOptions)
+
+    let markerOptions2 = {
+        position: new google.maps.LatLng(51.83191313142389, -3.3833262431141287)
+    }
+
+    let marker2 = new google.maps.Marker(markerOptions2)
+
+    marker.setMap(map);
+    marker2.setMap(map);
+}
+
+initMap();
+
